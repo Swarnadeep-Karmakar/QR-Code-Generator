25409381dd343697c5e66f5206479ea4275ca0c9
Swarnadeep Karmakar <79745402+Swarnadeep-Karmakar@users.noreply.github.com>
2024-02-24 02:54:30 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,46 @@
+import qrcode
+from tkinter import *
+
+cp = Tk()
+cp.title('QR Code Generator by Swarna')
+cp.geometry('700x250')
+cp.config(bg='#64abeb')
+
+def generate():
+    img = qrcode.make(msg.get())
+    type(img)
+    img.save(f'{save_name.get()}.png')
+    Label(cp, text='File Saved!', bg='#64abeb' , fg='black', font=('Times New Roman', 8)).pack()
+
+def show():
+    img = qrcode.make(msg.get())
+    type(img)
+    img.show()
+
+frame = Frame(cp, bg='#64abeb')
+frame.pack(expand=True)
+
+#------------------ENTER THE TEXT OR URL------------------
+
+Label(frame, text='Enter the Text or URL : ', font=('Times New Roman', 16),
+      bg='#64abeb').grid(row=0, column=0, sticky='w')
+
+msg = Entry(frame)
+msg.grid(row=0, column=1)
+
+#------------------ENTER THE FILE NAME------------------
+
+Label(frame, text='File Name(Save As) : ', font=('Times New Roman', 16),
+      bg='#64abeb').grid(row=1, column=0, sticky='w')
+
+save_name = Entry(frame)
+save_name.grid(row=1, column=1)
+
+#------------------BUTTONS TO SHOW OR SAVE QRCODE------------------
+
+btn = Button(cp, text='Show QR code', bd='5', command=show, width=15)
+btn.pack()
+btn = Button(cp, text='Save QR code', command=generate, bd='5', width=15)
+btn.pack()
+
+cp.mainloop()
